5fff519299403ac9ab97a866cbb3431000360d49
ansariali <=>
2025-05-07 21:32:03 +0530

    some file updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import React from "react";
 import { Footer } from "../components/footer/Footer";
+import Navbar from "../components/header/ResponsiveNav";
 
 type RootLayoutProps = {
   children: React.ReactNode;
@@ -10,8 +11,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>
-        <p>Header</p>
-        {children}
+        <Navbar />
+        <section className="mx-2 sm:mx-4 md:mx-12 lg:mx-18">{children}</section>
         <Footer />
       </body>
     </html>
--- src/components/Header/ResponsiveNav.jsx
+++ src/components/Header/ResponsiveNav.jsx
@@ -1,11 +1,22 @@
 import MobNav from "./MobNav";
 import Nav from "./Navbar";
+import { IoCartOutline } from "react-icons/io5";
 
 export default function Navbar() {
   return (
-    <section>
-      <Nav />
-      <MobNav />
-    </section>
+    <header className="flex items-center justify-between h-[8vh] sm:h-[10vh] bg-red-600 px-2 sm:px-4 md:px-12 lg:px-18 text-white">
+      <div className="font-bold uppercase text-[14px] sm:text-xl ">
+        Landing Page
+      </div>
+      <div>
+        <Nav />
+      </div>
+      <div>
+        <span>
+          <IoCartOutline />
+          <button>Buy Now</button>
+        </span>
+      </div>
+    </header>
   );
 }
